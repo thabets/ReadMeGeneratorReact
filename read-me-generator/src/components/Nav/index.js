edb566a59fc2164538afb11758636d61a77a4a1b
Sam Thabet <sallam.thabet@gmail.com>
2022-01-27 18:33:05 -0500

    added initial structure

---
+++ read-me-generator/src/components/Nav/index.js
@@ -0,0 +1,42 @@
+import React from "react";
+
+function Nav(props) {
+  const { home, setHome } = props;
+
+  return (
+    <header
+      className="flex-row px-2 py-2"
+      style={"background-image: linear-gradient(to right, red, blue);"}
+    >
+      <h2>
+        <a href="/" className="hbackground">
+          Read Me Generator
+        </a>
+      </h2>
+      <nav>
+        <ul className="flex-row ">
+          <li className="px-2 py-2 ">
+            <a
+              href="#about"
+              className="hbackground"
+              onClick={() => setHome("about")}
+            >
+              About
+            </a>
+          </li>
+          <li className="px-2 py-2">
+            <a
+              href="#portfolio"
+              className="hbackground"
+              onClick={() => setHome("portfolio")}
+            >
+              Application
+            </a>
+          </li>
+          
+        </ul>
+      </nav>
+    </header>
+    
+  );
+}
