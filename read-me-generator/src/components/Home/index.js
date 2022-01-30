e33cc8a5f89d6122e007038e1f314235856820df
Sam Thabet <sallam.thabet@gmail.com>
2022-01-29 20:33:46 -0500

    Created Home Page content

--- read-me-generator/src/components/Home/index.js
+++ read-me-generator/src/components/Home/index.js
@@ -1,19 +1,25 @@
-import React from "react";
+import  React, {useState } from "react";
 import { Card, Button } from "react-bootstrap";
 import "bootstrap/dist/css/bootstrap.min.css";
+import About from "../About/index"
 
-function Home() {
+function Home(props) {
+  const [ home, setHome ] = useState('home');
   return (
-    <Card className="text-center">
-      <Card.Body>
-        <Card.Title>Read Me Generator</Card.Title>
-        <Card.Text>
-          This is an application that will allow you to generate a Read Me file
-          for your projects on GitHub
-        </Card.Text>
-        <Button variant="primary"size="lg">Enter</Button>
-      </Card.Body>
-    </Card>
+    <div>
+      <Card className="text-center pd mar border">
+        <Card.Body>
+          <Card.Title>Read Me Generator</Card.Title>
+          <Card.Text>
+            This is an application that will allow you to generate a Read Me
+            file for your projects on GitHub
+          </Card.Text>
+          <Button variant="primary" size="lg" href="#about" onClick={() => setHome("about")}>
+            Enter
+          </Button>
+        </Card.Body>
+      </Card>
+    </div>
   );
 }
 
