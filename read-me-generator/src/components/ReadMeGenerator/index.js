f55cf52aa82af6dc9ac27f99113cf5d993ace160
Sam Thabet <sallam.thabet@gmail.com>
2022-01-30 21:50:35 -0500

    Started forms for the readme Generator

--- read-me-generator/src/components/ReadMeGenerator/index.js
+++ read-me-generator/src/components/ReadMeGenerator/index.js
@@ -1,35 +1,25 @@
 import React from "react";
+import { Form, Col, FloatingLabel } from "react-bootstrap";
 
 function ReadMeGenerator() {
-  
-
-
-
-
-
-
-
-
-
-
-  
+  return (
+    <div>
+      <Form>
+        <Col>
+          <Form.Control placeholder="Name of Project?" />
+          <Form.Control placeholder="Creator?" />
+          <Form.Control placeholder="Contributors" />
+          <FloatingLabel controlId="floatingTextarea" label="Description">
+            <Form.Control
+              as="textarea"
+              style={{ height: "100px" }}
+            />
+          </FloatingLabel>
+          
+        </Col>
+      </Form>
+    </div>
+  );
 }
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
 
 export default ReadMeGenerator;
