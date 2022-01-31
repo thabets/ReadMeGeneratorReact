667d98d1cd4504f2bd7e48e9fc1f1af28b254f14
Sam Thabet <sallam.thabet@gmail.com>
2022-01-31 00:15:12 -0500

    cosmetic fixes

--- read-me-generator/src/components/ReadMeGenerator/index.js
+++ read-me-generator/src/components/ReadMeGenerator/index.js
@@ -21,19 +21,28 @@ function ReadMeGenerator() {
             <Form.Control
               className="mar2"
               placeholder="Project Name"
+              style={{ width: "98%" }}
             ></Form.Control>
 
             <FormLabel className="label">
               Please Insert The Name Of The Creator
             </FormLabel>
 
-            <Form.Control className="mar2" placeholder="Creator" />
+            <Form.Control
+              className="mar2"
+              placeholder="Creator"
+              style={{ width: "98%" }}
+            />
 
             <FormLabel className="label">
               Please Insert The Name Of Any Contributors
             </FormLabel>
 
-            <Form.Control className="mar2" placeholder="Contributors" />
+            <Form.Control
+              className="mar2"
+              placeholder="Contributors"
+              style={{ width: "98%" }}
+            />
 
             <FormLabel className="label">
               Please Insert A Short Description Of The Application
@@ -60,14 +69,62 @@ function ReadMeGenerator() {
             </FloatingLabel>
           </Col>
           <Col>
-            <FloatingLabel controlId="floatingTextarea" label="Image Path">
+            <FormLabel className="label">
+              Please Insert The Paths To Your Image Within Github
+            </FormLabel>
+
+            <FloatingLabel
+              controlId="floatingTextarea3"
+              label="Image Path ex ../src/images/image.jpg"
+              className="mar2"
+            >
               <Form.Control as="textarea" style={{ height: "100px" }} />
             </FloatingLabel>
-            <Form.Control placeholder="Active Link" />
-            <Form.Control placeholder="Repository Link" />
-            <Form.Control placeholder="Contact Email" />
-            <Form.Control placeholder="Contact #" />
-            <Form.Select aria-label="License">
+
+            <FormLabel className="label">
+              Please Provide A Link To The Active Application/Website
+            </FormLabel>
+            <Form.Control
+              className="mar2"
+              placeholder="Active Link"
+              style={{ width: "98%" }}
+            ></Form.Control>
+
+            <FormLabel className="label">
+              Please Provide Repository Link
+            </FormLabel>
+            <Form.Control
+              className="mar2"
+              placeholder="Repository Link"
+              style={{ width: "98%" }}
+            />
+
+            <FormLabel className="label">
+              Please Provide Contact Information
+            </FormLabel>
+            <Form.Control
+              className="mar2"
+              placeholder="Contact Email"
+              style={{ width: "98%" }}
+            />
+
+            <FormLabel className="label">
+              Please Provide Contact Number
+            </FormLabel>
+            <Form.Control
+              className="mar2"
+              placeholder="Contact #"
+              style={{ width: "98%" }}
+            />
+
+            <FormLabel className="label">
+              Please Choose From The Listed Licenses For Your Project
+            </FormLabel>
+            <Form.Select
+              className="mar2"
+              aria-label="License"
+              style={{ width: "98%" }}
+            >
               <option>Please Select a License for this Project</option>
               <option value="1">MIT</option>
               <option value="2">IBM</option>
@@ -81,7 +138,7 @@ function ReadMeGenerator() {
           </Col>
         </Row>
       </Form>
-      <Button variant="dark" style={{margin:" 30px 10px 10px 10px"}}>
+      <Button variant="dark" style={{ margin: " 30px 10px 10px 10px" }}>
         Submit
       </Button>
     </div>
