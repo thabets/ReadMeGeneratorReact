d93ea7be9a6417bc05b81272417d1fe544ca42f7
Sam Thabet <sallam.thabet@gmail.com>
2022-02-01 22:25:01 -0500

    corrected function naming

--- read-me-generator/src/components/ReadMeGenerator/index.js
+++ read-me-generator/src/components/ReadMeGenerator/index.js
@@ -8,7 +8,8 @@ import {
   FormLabel,
 } from "react-bootstrap";
 
-import getVal from "../Helpers/index"
+import handleSubmit from "../Helpers/index"
+
 
 function ReadMeGenerator() {
   return (
@@ -16,40 +17,36 @@ function ReadMeGenerator() {
       <Form>
         <Row>
           <Col>
+            ""
             <FormLabel className="label">
               Please Insert The Name Of The Project/Application
             </FormLabel>
-
             <Form.Control
               className="mar2"
               placeholder="Project Name"
               style={{ width: "98%" }}
+              id="1"
             ></Form.Control>
-
             <FormLabel className="label">
               Please Insert The Name Of The Creator
             </FormLabel>
-
             <Form.Control
               className="mar2"
               placeholder="Creator"
               style={{ width: "98%" }}
+              id="2"
             />
-
             <FormLabel className="label">
               Please Insert The Name Of Any Contributors
             </FormLabel>
-
             <Form.Control
               className="mar2"
               placeholder="Contributors"
               style={{ width: "98%" }}
             />
-
             <FormLabel className="label">
               Please Insert A Short Description Of The Application
             </FormLabel>
-
             <FloatingLabel
               className="mar2"
               controlId="floatingTextarea"
@@ -57,11 +54,9 @@ function ReadMeGenerator() {
             >
               <Form.Control as="textarea" style={{ height: "100px" }} />
             </FloatingLabel>
-
             <FormLabel className="label">
               Please Insert Any Installation Requirements Or Instructions
             </FormLabel>
-
             <FloatingLabel
               controlId="floatingTextarea2"
               label="Installation Requirements Or Instructions"
@@ -143,7 +138,7 @@ function ReadMeGenerator() {
       <Button
         variant="dark"
         style={{ margin: " 30px 10px 10px 10px" }}
-        onClick={getVal}
+        onClick={handleSubmit}
       >
         Submit
       </Button>
