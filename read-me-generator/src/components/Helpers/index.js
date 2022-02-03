7b18a0eb1e7dca3726a0206ae9b4b9116202ac1d
Sam Thabet <sallam.thabet@gmail.com>
2022-02-02 22:02:32 -0500

    Added the readme document to be coppied still needs to be cleaned up

--- read-me-generator/src/components/Helpers/index.js
+++ read-me-generator/src/components/Helpers/index.js
@@ -1,8 +1,10 @@
 import React from "react";
 import { Card, Button } from "react-bootstrap";
 import { useNavigate } from "react-router-dom";
+import ReadMeGenerator from "../ReadMeGenerator";
 
-function handleSubmit() {
+function handleSubmit(ReadMeGenerator) {
+  const license = document.getElementById("11").value;
   const projectName = document.getElementById("1").value;
   const creator = document.getElementById("2").value;
   const contributors = document.getElementById("3").value;
@@ -13,109 +15,50 @@ function handleSubmit() {
   const repositoryLink = document.getElementById("8").value;
   const contactEmail = document.getElementById("9").value;
   const contactNum = document.getElementById("10").value;
-  const license = document.getElementById('11').value;
+  
   return (
     <div>
       <Card className="pd mar border">
-        <Card.Body>
-          <Card.Title>Your Generated ReadMe</Card.Title>
-          <Card.Text>
-            ${renderLicenseBadge(data.license)}
-
-# Table of Content
-## [Project Name](#project-name)
-## [Project Creator](#project-creator)
-## [Description](#description)
-## [Prerequisites for Use](#prerequisites)
-## [Installation](#installing)
-## [Screenshot](#screenshot)
-## [Active Url](#active-url)
-## [Repository Link](#repository-link)
-## [Contributing To](#contributing-to)
-## [Contributors](#contributors)
-## [Contact](#contact)
-## [license](#license)
-
-# Project name
-${data.title}
-# Project Creator
-${data.name}
-
-# Description
-Project name is \`${data.title}\` This application allows the user to ${
-    data.description
-  }.
-
-## Prerequisites
-
-Before you begin, ensure you have met the following requirements:
-
-* You have installed the latest version of Node.js
-* You have a Windows/Linux/Mac machine.
-* You have read guide/documentation_related_to_project>.
-
-## Installing
-
-To install <${data.title}>, follow these steps:
-
-${data.install}
-
-## Screenshot
-![Screenshot](${data.screenshot})
-
-## Active Url
-
-Here is the link to the active site: ${data.activeURL}
-
-## Repository Link
-
-Here is the link to the Github Repository: ${data.repository}
-
-## Contributing to
-To contribute to <${data.title}>, follow these steps:
-
-1. Fork this repository.
-2. Create a branch: \`git checkout -b <branch_name>\`.
-3. Make your changes and commit them: \`git commit -m '<commit_message>'\`
-4. Push to the original branch: \`git push origin <project_name>/<location>\`
-5. Create the pull request.
-
-Alternatively see the GitHub documentation on [creating a pull request](https://help.github.com/en/github/collaborating-with-issues-and-pull-requests/creating-a-pull-request).
-
-## Contributors
-
-Thanks to the following people who have contributed to this project:
-
-<${data.contributor}>
-
-## Contact
-
-If you want to contact me you can reach me at <${data.email}>.
-
-## License
-
-This project uses the following license: [${data.license}]${renderLicenseLink(
-    data.license
-  )}.
-
-        </Card.Text>
-        
-        
-        
+        <Card.Body className="text-center">
+          Please Copy Your Generated ReadMe Below for { projectName }
         </Card.Body>
-
-
-
-
-
-
-
-</Card>
-
-
-
-
-
+        <Card.Text>
+          { license }# Table of Content ## [Project Name](#project-name) ##
+          [Project Creator](#project-creator) ## [Description](#description) ##
+          [Prerequisites for Use](#prerequisites) ## [Installation](#installing)
+          ## [Screenshot](#screenshot) ## [Active Url](#active-url) ##
+          [Repository Link](#repository-link) ## [Contributing
+          To](#contributing-to) ## [Contributors](#contributors) ##
+          [Contact](#contact) ## [license](#license) # Project name
+          { projectName }# Project Creator
+          { creator }# Description
+          { description }
+          ## Prerequisites Before you begin, ensure you have met the following
+          requirements: * You have installed the latest version of Node.js * You
+          have a Windows/Linux/Mac machine. * You have read
+          guide/documentation_related_to_project. ## Installing To install{" "}
+          { projectName }, follow these steps:
+          { installReq }
+          ## Screenshot
+          {/* ![Screenshot](${data.screenshot}) */}
+          ## Active Url Here is the link to the active site: { ActiveLink }
+          ## Repository Link Here is the link to the Github Repository:{" "}
+          { repositoryLink }
+          ## Contributing to To contribute to { projectName }, follow these
+          steps: 1. Fork this repository. 2. Create a branch: \`git checkout -b
+          branch_name\`. 3. Make your changes and commit them: \`git commit -m
+          'commit_message'\` 4. Push to the original branch: \`git push origin{" "}
+          { projectName }/location\` 5. Create the pull request. Alternatively
+          see the GitHub documentation on [creating a pull
+          request](https://help.github.com/en/github/collaborating-with-issues-and-pull-requests/creating-a-pull-request).
+          ## Contributors Thanks to the following people who have contributed to
+          this project:
+          { contributors }
+          ## Contact If you want to contact me you can reach me at{" "}
+          { contactEmail } or { contactNum }. ## License This project uses
+          the following license: { license }
+        </Card.Text>
+      </Card>
     </div>
   );
 }
