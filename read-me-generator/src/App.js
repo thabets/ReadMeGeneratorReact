873029d1eb6aceddb3194bec0351f358647b77db
Sam Thabet <sallam.thabet@gmail.com>
2022-02-02 22:02:00 -0500

    Added the submit route

--- read-me-generator/src/App.js
+++ read-me-generator/src/App.js
@@ -5,6 +5,7 @@ import Nav from "./components/Nav/index";
 import Footer from "./components/Footer/index";
 import ReadMeGenerator from "./components/ReadMeGenerator/index";
 import Home from "./components/Home/index";
+import HandleSubmit from "./components/Helpers";
 import "bootstrap/dist/css/bootstrap.min.css";
 import { Routes, Route } from "react-router-dom";
 
@@ -24,6 +25,7 @@ function App() {
           <Route path="/" element={<Home />} />
           <Route path="/about" element={<About />} />
           <Route path="/readMeGenerator" element={<ReadMeGenerator />} />
+          <Route path="/handleSubmit" element={<HandleSubmit />}/>
         </Routes>
       </main>
       <Footer home={home} setHome={setHome}></Footer>
