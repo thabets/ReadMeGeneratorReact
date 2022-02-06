ea020d96df99b607a483ee3550402685aa3371b4
Sam Thabet <sallam.thabet@gmail.com>
2022-02-06 01:35:25 -0500

    Added Description

--- read-me-generator/src/components/Home/index.js
+++ read-me-generator/src/components/Home/index.js
@@ -3,18 +3,21 @@ import { Card, Button } from "react-bootstrap";
 import { useNavigate } from "react-router-dom"
 import "bootstrap/dist/css/bootstrap.min.css";
 
-
+//Home Page function/content
 function Home() {
+  //Navigational function to navigate the button to the next page
   const navigate = useNavigate();
   return (
     <div>
       <Card className="text-center pd mar border">
         <Card.Body>
           <Card.Title>Read Me Generator</Card.Title>
+          {/* Description of project */}
           <Card.Text>
             This is an application that will allow you to generate a Read Me
             file for your projects on GitHub
           </Card.Text>
+          {/* Entry button to the about page */}
           <Button variant="primary" size="lg" onClick={() => navigate("/about")} active>
             Enter
           </Button>
--- read-me-generator/src/components/Nav/index.js
+++ read-me-generator/src/components/Nav/index.js
@@ -1,12 +1,15 @@
 import React from "react";
+//Utilization of useNavigate for in page navigation 
 import { useNavigate } from "react-router-dom";
 
+//Navigational Bar function and render
 function Nav(props) {
   const navigate = useNavigate();
 
   return (
     <header
       className="flex-row px-2 py-2 header"
+      //Background gradient color image for Nav
       style={{
         backgroundImage: `linear-gradient(to right, rgba(165, 42, 42, 1), rgba(0, 133, 255, 1))`,
       }}
@@ -17,6 +20,7 @@ function Nav(props) {
         </a>
       </h2>
       <nav>
+        {/* Navigation Links */}
         <ul className="flex-row ">
           <li className="px-2 py-2 ">
             <a
